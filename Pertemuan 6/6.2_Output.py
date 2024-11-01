5c82878a445e7a23bd01d4aa39b2f1695e2ab236
wahyoe <145647812+bgwahyoe@users.noreply.github.com>
2024-11-01 15:03:00 +0700

    Pertemuan 6

---
+++ Pertemuan 6/6.2_Output.py
@@ -0,0 +1,10 @@
+a = 0
+bil_gan = 1 
+
+while bil_gan <= 20 :
+  a += bil_gan
+  bil_gan += 2
+
+print("1 + 3 + 5 + 7 +9 +11 +13 + 15 +17 +19 =", a)
+
+
---
+++ Pertemuan 6/6.3_bintang.py
@@ -0,0 +1,6 @@
+jumlah = int(input("Masukkan Jumlah baris :"))
+
+for i in range(1,jumlah + 1):
+  print("*" * i)
+  
+
